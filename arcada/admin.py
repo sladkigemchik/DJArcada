6a1a1512ae4e10c7b9b259f3c15cea2d2a353945
sladkigemchik <hoppi2hope@gmail.com>
2026-05-21 10:24:44 +0300

    fix lab9

--- arcada/admin.py
+++ arcada/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin, messages
+from django.utils.html import mark_safe
 from .models import GameArticle, Category, TagPost, UserProfile
 
 
@@ -22,21 +23,24 @@ class PriceFilter(admin.SimpleListFilter):
 
 @admin.register(GameArticle)
 class GameArticleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'game_name', 'price', 'status', 'cat', 'time_create', 'brief_info')
+    list_display = ('id', 'title', 'post_image', 'game_name', 'price', 'status', 'cat', 'time_create')
     list_display_links = ('title',)
     list_editable = ('status', 'price')
     ordering = ['-time_create']
     list_per_page = 10
     search_fields = ['title', 'game_name']
     list_filter = ['cat', 'status', 'time_create', PriceFilter]
-    fields = ['title', 'slug', 'game_name', 'genre', 'price', 'content', 'cat', 'tags', 'status']
+    fields = ('title', 'slug', 'game_name', 'genre', 'price', 'content', 'image', 'post_image', 'cat', 'tags', 'status')
+    readonly_fields = ('post_image',)
     prepopulated_fields = {'slug': ('title',)}
     filter_horizontal = ['tags']
     actions = ['make_published', 'make_draft']
 
-    @admin.display(description="Краткое описание")
-    def brief_info(self, obj):
-        return f"Текст: {len(obj.content)} симв."
+    @admin.display(description="Изображение")
+    def post_image(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="50" height="50" style="object-fit: cover; border-radius: 8px;" />')
+        return "Нет фото"
 
     @admin.action(description="Опубликовать выбранные записи")
     def make_published(self, request, queryset):
@@ -66,6 +70,7 @@ class TagPostAdmin(admin.ModelAdmin):
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'bio', 'phone')
+
 
 admin.site.site_header = "Панель управления Arcada Project"
 admin.site.site_title = "Arcada Admin"
